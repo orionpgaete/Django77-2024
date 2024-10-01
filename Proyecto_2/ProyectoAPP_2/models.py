4ef003f5a13246f1eeabff7b2d5380935409ce85
orionpgaete <pedro.gaete@pgcomputer.cl>
2024-10-01 20:08:31 -0300

    Proyecto Inicial BD

---
+++ Proyecto_2/ProyectoAPP_2/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+    tfno = models.IntegerField()
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=20)
+    precio= models.IntegerField()
+
+class Pedido(models.Model):
+    nropedido=models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
+
+#  python manage.py makemigrations
+
+# python3 manage.py migrate
